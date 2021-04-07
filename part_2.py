3bfcb6a65a54566feb441b44f0b98f8712b552ab
Umang Srivastava <umang.srivastava@students.iiit.ac.in>
2021-04-07 19:33:11 +0530

    Updated part_2

--- part_2.py
+++ part_2.py
@@ -4,27 +4,32 @@ import os
 import random
 import numpy as np
 # from dict import DotDict, npdict
-
-# TEAM 44 - YogiJi
 GAMMA = 0.999
 DELTA = 1e-3
-Y = [1/2, 1, 2][44 % 3]
-STEP_COST = -10/Y
+FL = 2
+# TEAM 44 - YogiJi
+Y = [1/2, 1, 2]
+STEP_COST = -5
 MOVEMENT_PROBABILITY = 0.85
 HIT_PROBABILITY = 0.75
 HEALING_POINTS = 25
 MONSTER_STATE_CHANGE_PROB = 0.2
 ATTACK_MONSTER_PROBABILITY = 0.5
 MONSTER_DAMAGE = 25
-
-
-def probabilityTF(prob: float):
-    tp = random.random()
-    if tp >= prob:
-        return False
-    else:
-        return True
-
+ARROW_HIT_PROB_E = 0.9
+ARROW_HIT_PROB_C = 0.5
+ARROW_HIT_PROB_W = 0.25
+PROB_1_ARROW = 0.5
+PROB_2_ARROW = 0.35
+PROB_3_ARROW = 0.15
+BLADE_HIT_PROB_E = 0.2
+BLADE_HIT_PROB_C = 0.1
+#       W    N     C    S    E
+#   L
+#   U
+#   S
+#   D
+#   R
 class DotDict(OrderedDict):
     def __getattr__(*args):
         val = dict.get(*args)
@@ -49,14 +54,15 @@ class npdict():
         self.arr[self.mapper[key]] = val
 
 
+
 PlayerAttacks = DotDict({
-    "SHOOT": 25, 
+    "SHOOT": 25,
     "HIT": 50,
 })
 
 PlayerActions = DotDict({
-    "CRAFT": 2, 
-    "GATHER": 3, 
+    "CRAFT": 2,
+    "GATHER": 3,
     "NONE": 4,
 })
 
@@ -68,13 +74,13 @@ class PlayerTakeStep:
     RIGHT = np.array((1, 0))
     STAY = np.array((0, 0))
 
-PlayerMoves = DotDict({
-    "UP": tuple(PlayerTakeStep.UP),
-    "DOWN": tuple(PlayerTakeStep.DOWN),
-    "LEFT": tuple(PlayerTakeStep.LEFT),
-    "RIGHT": tuple(PlayerTakeStep.RIGHT),
-    "STAY": tuple(PlayerTakeStep.STAY),
 
+PlayerMoves = DotDict({
+    "LEFT": tuple(PlayerTakeStep.LEFT),
+    "UP": tuple(PlayerTakeStep.UP),
+    "STAY": tuple(PlayerTakeStep.STAY),
+    "DOWN": tuple(PlayerTakeStep.DOWN),
+    "RIGHT": tuple(PlayerTakeStep.RIGHT),
 })
 
 class StateOfPlayer:
@@ -85,169 +91,44 @@ class StateOfPlayer:
     E = np.array((1, 0))
 
 PlayerStates = DotDict({
+    "W": tuple(StateOfPlayer.W),
     "N": tuple(StateOfPlayer.N),
+    "C": tuple(StateOfPlayer.C),
     "S": tuple(StateOfPlayer.S),
     "E": tuple(StateOfPlayer.E),
-    "W": tuple(StateOfPlayer.W),
-    "C": tuple(StateOfPlayer.C),
 })
 
 
 _INV_STATE_MAP = {PlayerStates[k]: k for k in PlayerStates}
 _INV_STATE_MAP_INDEX = {k: i for i, k in enumerate(PlayerStates)}
-
+_INV_ACTIONS_MAP = {PlayerActions[k]: k for k in PlayerActions}
+_INV_ACTIONS_MAP.update({PlayerAttacks[k]: k for k in PlayerAttacks})
+_INV_ACTIONS_MAP.update({PlayerMoves[k]: k for k in PlayerMoves})
 
 class IndexOfState:
-    W = 0
-    N = 1
-    C = 2
-    S = 3
-    E = 4
+    # W = 0
+    # N = 1
+    # C = 2
+    # S = 3
+    # E = 4
     _inv_map = DotDict({
-        tuple(StateOfPlayer.N): N,
-        tuple(StateOfPlayer.S): S,
-        tuple(StateOfPlayer.E): E,
-        tuple(StateOfPlayer.W): W,
-        tuple(StateOfPlayer.C): C,
-
+        tuple(StateOfPlayer.W): 0,
+        tuple(StateOfPlayer.N): 1,
+        tuple(StateOfPlayer.C): 2,
+        tuple(StateOfPlayer.S): 3,
+        tuple(StateOfPlayer.E): 4,
     })
 
+    # @staticmethod
     def source_state(state):
         return IndexOfState._inv_map[tuple(state)]
 
-_INV_ACTIONS_MAP = {PlayerActions[k]: k for k in PlayerActions}
-_INV_ACTIONS_MAP.update({PlayerMoves[k]: k for k in PlayerMoves})
-_INV_ACTIONS_MAP.update({PlayerAttacks[k]: k for k in PlayerAttacks})
-
-
-class IJones(object):
-    MOVES = PlayerMoves
-    STATES = PlayerStates
-    ATTACKS = PlayerAttacks
-    ACTIONS = PlayerActions
-
-    def __init__(self, inital_state=StateOfPlayer.N):
-        self.state = inital_state
-        self._action = IJones.ACTIONS.NONE
-        self.arrows = 0
-        self.materials = 0
-        self.reward = 0
-        self.stunned = False
-        self._values = np.zeros((5,), dtype=np.float64)
-
-    def move(self, direction):
-        # add direction to state
-        self.state += direction
-        if np.sum(np.abs(self.state)) > 1:
-            # out of bounds, illegal move -> undo move
-            self.state -= direction
-
-    # @property
-    def current_state(self) -> str:
-        return _INV_STATE_MAP[tuple(self.state)]
-
-    # @property
-    def action(self) -> str:
-        return _INV_ACTIONS_MAP[self._action]
-
-    @property
-    def values(self):
-        return npdict(self._values, _INV_STATE_MAP_INDEX)
-
-    def move_to_E(self):
-        self.state = StateOfPlayer.E
-
-    def try_move(self, direction: tuple):
-        if tuple(self.state) in [IJones.STATES.C, IJones.STATES.N, IJones.STATES.S]:
-            if not probabilityTF(MOVEMENT_PROBABILITY):
-                self.move_to_E()
-                return
-        # any other move for any state is determined i.e. prob = 1
-        self.move(direction)
-
-    def value_iteration(self):
-        state_index = IndexOfState.source_state(self.state)
-        rs = np.zeros((5,), dtype=np.float64)
-        fxs = np.zeros((5,), dtype=np.float64)
-        for i in range(5):
-            rs[i] = np.sum(player_board[state_index][i] * STEP_COST)
-            fxs[i] = GAMMA * np.sum(self._values.copy() * player_board[state_index][i])
-        lst = rs + fxs
-        maxcv = np.max(lst)
-        self._values[state_index] = maxcv
-
-    def get_wrecked(self):
-        if tuple(self.state) not in [IJones.STATES.C, IJones.STATES.E]:
-            return
-        self.arrows = 0
-        self.reward -= 40
-        self.stunned = True
-
-    def attackMonster(self, enemy, action: int):
-        temp = 1
-        if action == IJones.ATTACKS.SHOOT:
-            if self.arrows == 0:
-                return
-            self.arrows -= 1
-            if np.array_equal(self.state, StateOfPlayer.C):
-                temp = 0.5
-            elif np.array_equal(self.state, StateOfPlayer.W):
-                temp = 0.25
-            elif np.array_equal(self.state, StateOfPlayer.E):
-                temp = 0.9
-        elif action == IJones.ATTACKS.HIT:
-            if np.array_equal(self.state, StateOfPlayer.C):
-                temp = 0.1
-            elif np.array_equal(self.state, StateOfPlayer.E):
-                temp = 0.2
-
-        # deal appropriate damage with success_probability
-        if probabilityTF(temp):
-            enemy.monsterAttacked(action)
-
-    def craft(self):
-        if not np.array_equal(self.state, IJones.STATES.N) or self.materials == 0:
-            return
-        self.materials -= 1
-        self.arrows += np.random.choice([1, 2, 3], p=[0.5, 0.35, 0.15])
-
-    def gather(self):
-        if not np.array_equal(self.state, IJones.STATES.S):
-            return
-        if probabilityTF(HIT_PROBABILITY):
-            if self.materials == 0:
-                self.materials += 1
-
-    def take_step(self, enemy):
-        self.value_iteration()
-        if self.stunned:
-            # can't make any move for one round
-            self.stunned = False
-            return
-        if not hasattr(self, 'choices'):
-            # all possible choices player could make
-            self.choices = []
-            self.choices.extend(list(IJones.ATTACKS.values()))
-            self.choices.extend(list(IJones.MOVES.values()))
-            self.choices.extend(list(IJones.ACTIONS.values()))
-        # perform a random action
-        self._action = random.choice(self.choices)
-        self.perform_action(enemy)
-
-    def perform_action(self, enemy):
-        action = self._action
-        if action in IJones.ATTACKS.values():
-            self.attackMonster(enemy, action)
-        elif action in IJones.MOVES.values():
-            self.try_move(random.choice(list(IJones.MOVES.values())))
-        elif action in IJones.ACTIONS.values():
-            if action == IJones.ACTIONS.CRAFT:
-                self.craft()
-            elif action == IJones.ACTIONS.GATHER:
-                self.gather()
-            elif action == IJones.ACTIONS.NONE:
-                pass
-
+def probabilityTF(prob: float):
+    tp = random.random()
+    if tp >= prob:
+        return False
+    else:
+        return True
 
 class MightyMonster(object):
     STATES = DotDict({"D": 0, "R": 1})
@@ -258,23 +139,173 @@ class MightyMonster(object):
     def monsterAttacked(self, damage: int = MONSTER_DAMAGE):
         self.health -= damage
 
-    def try_attackMonster(self, player: IJones):
+    def monster_attack_ij(self, player):
         if probabilityTF(ATTACK_MONSTER_PROBABILITY):
             # attackMonster
-            player.get_wrecked()
-            self.health += HEALING_POINTS  # healing of monster
-            self.state = MightyMonster.STATES.D  # change monster state to dormant
+            if player.monster_attack_successful() == True:
+                print("Monster attacked")
+                self.health += HEALING_POINTS  # healing of monster
+                self.health = min(self.health, 100)
+                self.state = MightyMonster.STATES.D  # change monster state to dormant
 
-    def take_step(self, player: IJones):
-        if probabilityTF(MONSTER_STATE_CHANGE_PROB): 
+    def take_step(self, player):
+        if probabilityTF(MONSTER_STATE_CHANGE_PROB):
             self.state = MightyMonster.STATES.R  # change monster state to ready
         if self.state == MightyMonster.STATES.R:
-            self.try_attackMonster(player)
+            self.monster_attack_ij(player)
+
+class IJones(object):
+    
+    ATTACKS = PlayerAttacks
+    ACTIONS = PlayerActions
+    MOVES = PlayerMoves
+    STATES = PlayerStates
+
+    def __init__(self):
+        # self.name = "Indiana Jones"
+        # ATTACKS = PlayerAttacks
+        # ACTIONS = PlayerActions
+        # MOVES = PlayerMoves
+        # STATES = PlayerStates
+        self.state = StateOfPlayer.N
+        self._action = IJones.ACTIONS.NONE
+        self.ij_step_flag = False
+        self._values = np.zeros((5,), dtype=np.float64)
+        self.arrows = 0
+        self.materials = 0
+        self.reward = 0
+
+    def current_state(self) -> str:
+        return _INV_STATE_MAP[tuple(self.state)]
+
+    def action(self) -> str:
+        return _INV_ACTIONS_MAP[self._action]
+
+    def move_to_E(self):
+        self.state = StateOfPlayer.E
+
+    def move(self, d):
+        self.state += d
+        if np.sum(np.abs(self.state)) > 1:
+            self.state -= d
+
+    def execute_step(self, d: tuple):
+        if not probabilityTF(FL):
+            return
+        if tuple(self.state) in [IJones.STATES.C, IJones.STATES.N, IJones.STATES.S]:
+            if probabilityTF(MOVEMENT_PROBABILITY):
+                pass
+            else:
+                self.move_to_E()
+                return
+        self.move(d)
+
+    def take_step(self, monster):
+        # self.value_iteration()
+        # VALUE ITERATION
+        val1 = np.zeros((5,), dtype=np.float64)
+        val2 = np.zeros((5,), dtype=np.float64)
+        state_index = IndexOfState.source_state(self.state)
+        val1[0] = np.sum(player_board[state_index][0] * STEP_COST)
+        val2[0] = GAMMA * np.sum(self._values.copy() * player_board[state_index][0])
+        val1[1] = np.sum(player_board[state_index][1] * STEP_COST)
+        val2[1] = GAMMA * np.sum(self._values.copy() * player_board[state_index][1])
+        val1[2] = np.sum(player_board[state_index][2] * STEP_COST)
+        val2[2] = GAMMA * np.sum(self._values.copy() * player_board[state_index][2])
+        val1[3] = np.sum(player_board[state_index][3] * STEP_COST)
+        val2[3] = GAMMA * np.sum(self._values.copy() * player_board[state_index][3])
+        val1[4] = np.sum(player_board[state_index][4] * STEP_COST)
+        val2[4] = GAMMA * np.sum(self._values.copy() * player_board[state_index][4])
+        # lst = val1 + val2
+        # maxcv = np.max(val1 + val2)
+        self._values[state_index] = np.max(val1 + val2)
+        # self._values[:] = np.round(self._values, 9)
+        if self.ij_step_flag == True:
+            self.ij_step_flag = False
+            return
+        if hasattr(self, 'choices'):
+            pass
+        else:
+            # all possible choices player could make
+            self.choices = []
+            self.choices.extend(list(IJones.ATTACKS.values()))
+            self.choices.extend(list(IJones.MOVES.values()))
+            self.choices.extend(list(IJones.ACTIONS.values()))
+        # perform a random action
+        self._action = random.choice(self.choices)
+        # self.perform_action(monster)
+        # action = self._action
+        if self._action in IJones.ATTACKS.values():
+            self.attackMonster(monster, self._action)
+        elif self._action in IJones.MOVES.values():
+            self.execute_step(random.choice(list(IJones.MOVES.values())))
+        elif self._action in IJones.ACTIONS.values():
+            if self._action == IJones.ACTIONS.CRAFT:
+                self.craft()
+            elif self._action == IJones.ACTIONS.GATHER:
+                self.gather()
+        return
+
+    @property
+    def values(self):
+        return npdict(self._values, _INV_STATE_MAP_INDEX)
+
+    def monster_attack_successful(self):
+        tp_flag = False
+        if tuple(self.state) not in [IJones.STATES.C, IJones.STATES.E]:
+            pass
+        else:
+            self.arrows = 0
+            self.reward -= 40
+            self.ij_step_flag = True
+            tp_flag = True
+        return tp_flag
+
+    def gather(self):
+        # if not np.array_equal(self.state, IJones.STATES.S):
+            
+        if np.array_equal(self.state, IJones.STATES.S) and probabilityTF(HIT_PROBABILITY):
+            if self.materials == 0:
+                self.materials += 1
+
+    def attackMonster(self, monster, action: int):
+        prob = 1
+
+        if action == IJones.ATTACKS.HIT:
+            if np.array_equal(self.state, StateOfPlayer.C):
+                prob = BLADE_HIT_PROB_C
+            elif np.array_equal(self.state, StateOfPlayer.E):
+                prob = BLADE_HIT_PROB_E
+        elif action == IJones.ATTACKS.SHOOT:
+            self.arrows -= 1
+            if self.arrows <= 0:
+                return
+            if np.array_equal(self.state, StateOfPlayer.E):
+                prob = ARROW_HIT_PROB_E
+            elif np.array_equal(self.state, StateOfPlayer.C):
+                prob = ARROW_HIT_PROB_C
+            elif np.array_equal(self.state, StateOfPlayer.W):
+                prob = ARROW_HIT_PROB_W
+
+        if probabilityTF(prob):
+            print("Player attacked")
+            monster.monsterAttacked(action)
+
+    def craft(self):
+        if np.array_equal(self.state, IJones.STATES.N) and self.materials != 0:
+        #     pass
+        # else:
+            self.arrows += np.random.choice([1, 2, 3], p=[PROB_1_ARROW, PROB_2_ARROW, PROB_3_ARROW])
+            self.materials -= 1
+
+    
 def main():
+    os.makedirs("outputs", exist_ok=True)
+    # print("Params", "DELTA", DELTA, "GAMMA", GAMMA, "STEP_COST", STEP_COST)
     # file = open("trace1.txt", "w")
     IJ = IJones()
     monster = MightyMonster()
-    max_moves = 1000
+    max_moves = 2000
     for iter_count in range(1,max_moves+1):
         # iter_count += 1
         # temp = "iteration="+ str(iter_count)+"\n"
@@ -284,60 +315,54 @@ def main():
         IJ.take_step(monster)
         # file.write(f"({IJ.current_state()},{IJ.materials},{IJ.arrows},{monster.state},{monster.health}):{IJ.action()}=[{IJ.values[IJ.current_state()]}]\n")
         print(f"({IJ.current_state()},{IJ.materials},{IJ.arrows},{monster.state},{monster.health}):{IJ.action()}=[{IJ.values[IJ.current_state()]}]")
+        print("Monster health = " + str(monster.health))
         if monster.health <= 0:
             break
 
     return
 
-#       W    N     C    S    E
-#   L
-#   U
-#   S
-#   D
-#   R
 player_board = np.array([
     [
     #W
-        [.00, .00, .00, .00, .00],
-        [.00, .00, .00, .00, .00],
-        [1.0, .00, .00, .00, .00],
-        [.00, .00, .00, .00, .00],
-        [.00, .00, 1.0, .00, .00],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
     ],
     [
     #N
-        [.00, .00, .00, .00, .00],
-        [.00, .00, .00, .00, .00],
-        [.00, .85, .00, .00, .15],
-        [.00, .00, .85, .00, .15],
-        [.00, .00, .00, .00, .00],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0.85, 0, 0, 0.15],
+        [0, 0, 0.85, 0, 0.15],
+        [0, 0, 0, 0, 0],
     ],
     [
     #C
-        [.85, .00, .00, .00, .15],
-        [.00, .85, .00, .00, .15],
-        [.00, .00, .85, .00, .15],
-        [.00, .00, .00, .85, .15],
-        [.00, .00, .00, .00, 1.0],
+        [0.85, 0, 0, 0, 0.15],
+        [0, 0.85, 0, 0, 0.15],
+        [0, 0, 0.85, 0, 0.15],
+        [0, 0, 0, 0.85, 0.15],
+        [0, 0, 0, 0, 1],
     ],
     [
     #S
-        [.00, .00, .00, .00, .00],
-        [.00, .00, .85, .00, .15],
-        [.00, .00, .00, .85, .15],
-        [.00, .00, .00, .00, .00],
-        [.00, .00, .00, .00, .00],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0.85, 0, 0.15],
+        [0, 0, 0, 0.85, 0.15],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
     ],
     [
     #E
-        [.00, .00, 1.0, .00, .00],
-        [.00, .00, .00, .00, .00],
-        [.00, .00, .00, .00, 1.0],
-        [.00, .00, .00, .00, .00],
-        [.00, .00, .00, .00, .00],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
     ],
 ])
 
-# os.makedirs("outputs", exist_ok=True)
 
 main()
